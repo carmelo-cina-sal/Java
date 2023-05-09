f52c73297b88481f7666bb9fa2b4eba6d0b86b35
carmelo-cina-sal <129075486+carmelo-cina-sal@users.noreply.github.com>
2023-05-09 17:40:54 +0200

    Prima commit

---
+++ Operazioni/File2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Fila2 {
+
+    public static void main(String[] args) {
+      int scelta = 0;
+      System.out.println("Selezionare l'operazione da effettuare");
+      System.out.println("1-somma tra due numeri");
+      System.out.println("2-differenza tra due numeri");
+      System.out.println("3-prodotto tra due numeri");
+      System.out.println("4-divisione tra due numeri");
+      System.out.println("5-quadrato di un numero");
+      System.out.println("6-cubo di un numero");
+      Scanner in = new Scanner(System.in);
+      scelta = in.nextInt();
+      
+      Fila2 f2 = new Fila2();
+      
+      switch (scelta){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        default: f1.stampaMessaggio("Funzionalità non prevista o ancora mancante.");
+      }
+    }
+    
+    void stampaMessaggio(String m){
+      System.out.println(m);
+    }
+
+}
---
+++ Operazioni/File4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Fila4 {
+
+    public static void main(String[] args) {
+      int scelta = 0;
+      System.out.println("Selezionare l'operazione da effettuare");
+      System.out.println("1-somma tra due numeri");
+      System.out.println("2-differenza tra due numeri");
+      System.out.println("3-prodotto tra due numeri");
+      System.out.println("4-divisione tra due numeri");
+      System.out.println("5-quadrato di un numero");
+      System.out.println("6-cubo di un numero");
+      Scanner in = new Scanner(System.in);
+      scelta = in.nextInt();
+      
+      Fila4 f4 = new Fila4();
+      
+      switch (scelta){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        default: f4.stampaMessaggio("Funzionalità non prevista o ancora mancante.");
+      }
+    }
+    
+    void stampaMessaggio(String m){
+      System.out.println(m);
+    }
+
+}
